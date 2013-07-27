d8124dfea0858515828d761801796ce19afaf405
Matthew Radcliffe <mradcliffe@softpixel.com>
2013-07-27 15:15:04 -0400

    Add CLI php file to run the challenge.

---
+++ run.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * run.php
+ *
+ * PHP Code Jam challange.
+ */
+
+$loader = require 'vendor/autoload.php';
+$loader->register();
+
+use PokerHand\PokerHand;
+use PokerHand\Feed\PokerHandFeedColumbusPHP;
+use PokerHand\Collection\PokerHandCollection;
+
+// Get a poker hand collection and rank 'em.
+$hands = PokerHandCollection::createFromFeed(new PokerHandFeedColumbusPHP);
+
+$hands->rankHands()->sortHands();
+
+$winner = TRUE;
+foreach ($hands->hands as $player => $hand) {
+  if ($winner) {
+    print 'Winner: ' . $player . "\n\n";
+  }
+  
+  print "\t$player: " . $hand->__toString() . "\t(" . $hand::$ranks[$hand->hand_rank] . ")\n";
+  $winner = FALSE;
+}
