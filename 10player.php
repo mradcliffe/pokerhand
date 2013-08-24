87811578edd1e6e976d6951ef16204c9a7977681
Matthew Radcliffe <mradcliffe@softpixel.com>
2013-08-24 15:25:17 -0400

    Issue None by mradcliffe: Demonstrate PokerHandFeedGenerator class with 10-player poker game.

---
+++ 10player.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file
+ * run.php.
+ */
+
+$loader = require 'vendor/autoload.php';
+$loader->register();
+
+use PokerHand\PokerHand;
+use PokerHand\Feed\PokerHandFeedGenerator;
+use PokerHand\Collection\PokerHandCollection;
+
+$game = PokerHandCollection::createFromFeed(PokerHandFeedGenerator::createGameOf(10));
+
+$game->rankHands()->sortHands();
+
+$winner = TRUE;
+foreach ($game->hands as $player => $hand) {
+  if ($winner) {
+    print 'Winner: ' . $player . "\n\n";
+  }
+  
+  print "\t$player: " . $hand->__toString() . "\t(" . $hand::$ranks[$hand->hand_rank] . ")\n";
+  $winner = FALSE;
+}
